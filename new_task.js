857a4bc4f391d4a368a8eec5996db58903ed54f9
Allan <allanoliveira.main@gmail.com>
2019-03-05 02:28:03 -0300

    setting up a work queue

--- new_task.js
+++ new_task.js
@@ -7,12 +7,12 @@ amqp.connect('amqp://localhost', function(err, conn) {});
 amqp.connect('amqp://localhost', function(err, conn) {
   if (err) throw err;
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'task_queue';
+    var msg = process.argv.slice(2).join(' ') || "Hello World!";
 
-    ch.assertQueue(q, {durable: false});
-    // Note: on Node 6 Buffer.from(msg) should be used
-    ch.sendToQueue(q, new Buffer('Hello World!'));
-    console.log(" [x] Sent 'Hello World!'");
+    ch.assertQueue(q, {durable: true});
+    ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+    console.log(" [x] Sent '%s'", msg);
     setTimeout(function() { conn.close(); process.exit(0) }, 500);
   });
 });
